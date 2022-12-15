200a180e837a8a03be7cf647b262ca4598d82520
Dima <dmitry.avdeenko@gmail.com>
2022-12-15 21:21:38 +0100

    Added person class, name

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,27 @@
 package com.example.demo;
 
+import com.example.demo.entity.Person;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         SpringApplication.run(DemoApplication.class, args);
+
+        Person person1 = new Person("Dima", 66);
+        Person person2 = new Person("Dimon", 77);
+        Person person3 = new Person("Dimons", 88);
+
+        System.out.println(person3.getFirstName());
+
+        person3.setFirstName("Max");
+
+        System.out.println(person3.getFirstName());
+
+
     }
 
 }
